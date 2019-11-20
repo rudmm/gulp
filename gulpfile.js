550e975256906a38cb25f7b466888710124faada
Mykhailo Rud <rudmm@ukr.net>
2019-11-20 22:29:27 +0200

    finish

--- gulpfile.js
+++ gulpfile.js
@@ -9,8 +9,11 @@ const rigger = require('gulp-rigger');
 const htmlMin = require('gulp-htmlmin');
 const sourcemaps = require('gulp-sourcemaps');
 const sass = require('gulp-sass');
-const imageResize = require('gulp-image-resize');
 const rename = require('gulp-rename');
+const responsive = require('gulp-responsive');
+const retina = require('gulp-img-retina');
+const plumber = require('gulp-plumber');
+const gutil = require('gutil');
 
 gulp.task('clean', () => {
     return del(['build/*'])
@@ -50,27 +53,61 @@ gulp.task('watch', () => {
     gulp.watch('./src/styles/**/*.scss', gulp.series('styles'))
     gulp.watch('./src/js/**/*.js', gulp.series('scripts'))
     gulp.watch('./src/**/*.html', gulp.series('html'))
-    gulp.watch('./src/img/**', gulp.series('img-compress', 'img', 'img-svg'))
+    gulp.watch('./src/img/**', gulp.series('img-compress', 'img-svg'))
 });
 
 gulp.task('html', () => {
     return gulp.src('./src/*.html')
         .pipe(rigger())
+        .pipe(retina({
+            1: '',
+            2: '@2x'
+        }))
         .pipe(htmlMin({ collapseWhitespace: true }))
         .pipe(gulp.dest('./build/'))
         .pipe(browserSync.stream())
 });
 
+
 gulp.task('img-compress', () => {
     return gulp.src('./src/img/**/*.{png,jpg}')
-        .pipe(imageResize({
-            width : '50%',
-            height : '50%',
-            crop : false,
-            upscale : false
-        }))
-        .pipe(gulp.dest('./build/img/'))
-        .pipe(browserSync.stream())
+        .pipe(plumber())
+       .pipe(
+           responsive(
+               {
+                   '*.{png,jpg}':[
+                       {
+                           width: '50%'
+                       },
+                       {
+                           width: '100%',
+                           rename:{
+                               suffix: '@2x'
+                           }
+                       }
+                   ]
+               },
+               {
+                   // Global configuration for all images
+                   // The output quality for JPEG, WebP and TIFF output formats
+                   quality: 50,
+                   // Use progressive (interlace) scan for JPEG and PNG output
+                   progressive: true,
+                   // Zlib compression level of PNG output format
+                   compressionLevel: 6,
+                   // Strip all metadata
+                   withMetadata: false,
+                   errorOnUnusedImage: false
+               }
+           )
+
+
+       )
+        .on('error', function (err) {
+            gutil.log('No matching images found in source: ' +'./src/img/**/*.{png,jpg}' , '');
+        })
+       .pipe(gulp.dest('./build/img/'))
+       .pipe(browserSync.stream())
 });
 
 gulp.task('img-svg', () => {
@@ -91,7 +128,6 @@ gulp.task('fonts', () => {
         .pipe(gulp.dest('build/fonts/'))
         .pipe(browserSync.stream())
 });
-
-gulp.task('build', gulp.series('clean', gulp.parallel('html', 'styles', 'scripts', gulp.series('img-compress', 'img', 'img-svg') , 'fonts')));
+gulp.task('build', gulp.series('clean', gulp.parallel('html', 'styles', 'scripts', gulp.series('img-compress', 'img-svg'), 'fonts')));
 
 gulp.task('default', gulp.series('build', 'watch'));
